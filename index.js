2dce8d426aa2b13bdb6d98b7e13e870cbaa7778f
Ralph Nicholson <nicholson.re@gmail.com>
2023-04-10 10:58:36 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let guests = [];
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+        guests.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return guests;
+}
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+}
